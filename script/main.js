b8c5ec18672887d3bd9663cfeb8ec6c344df4e28
lpcodepro <81183369+lpcodepro@users.noreply.github.com>
2021-04-10 18:00:59 +0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".slider").slick({dots:!0,infinite:!0,speed:700,slidesToShow:1})}),AOS.init({disable:!1,startEvent:"DOMContentLoaded",initClassName:"aos-init",animatedClassName:"aos-animate",useClassNames:!1,disableMutationObserver:!1,debounceDelay:50,throttleDelay:99,offset:120,delay:0,duration:400,easing:"ease",once:!0,mirror:!1,anchorPlacement:"top-bottom"});
